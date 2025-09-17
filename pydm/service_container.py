72a532c97eb26f120e5bdcccdd0adcf4cefec57d
Ariana Maghsoudi <ariana.maghsoudi82@gmail.com>
2025-09-17 13:37:56 +0330

    feat: Implement singleton pattern and auto-wiring

--- pydm/service_container.py
+++ pydm/service_container.py
@@ -1,8 +1,44 @@
-from typing import TypeVar, Type
+from inspect import signature
+from typing import TypeVar, Type, Dict
+from underpy import Encapsulated
 
 T = TypeVar("T")
 
-class ServiceContainer:
+class ServiceContainer(Encapsulated):
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls._instance = super(ServiceContainer, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, "__services"):
+            self.__services: dict[Type[T], T] = {}
+
     @classmethod
-    def get(cls, name: Type) -> T:
-        return
+    def get_instance(cls) -> 'ServiceContainer':
+        if not cls.__instance:
+            cls.__instance = cls()
+        return cls.__instance
+
+    def get_service(self, cls: Type[T]) -> T:
+        if cls in self.__services:
+            return self.__services[cls]
+
+        dependencies: dict[str, T] = {}
+        arguments = signature(cls.__init__).parameters
+        for arg_name, arg in arguments.items():
+            if 'self' == arg_name or arg.VAR_POSITIONAL == arg.kind or arg.VAR_KEYWORD == arg.kind:
+                continue
+
+            arg_cls = arg.annotation
+            if arg.empty == arg_cls:
+                raise ValueError(f"Dependency '{arg_name}' in '{cls.__name__}' constructor is missing a type hint.")
+
+            dependencies[arg_name] = self.get_service(arg_cls)
+
+        instance = cls(**dependencies)
+        self.__services[cls] = instance
+
+        return instance
--- tests/test_service_container.py
+++ tests/test_service_container.py
@@ -1,20 +1,36 @@
 from assertpy.assertpy import assert_that
 from pydm import ServiceContainer
 
-
-class DummyRepository:
+class DummyUnitOfWork:
     pass
-
+class RepositoryStub:
+    def __init__(self, unit_of_work: DummyUnitOfWork):
+        self.unit_of_work = unit_of_work
 class UseCaseStub:
-    def __init__(self, repository: ServiceContainer):
+    def __init__(self, repository: RepositoryStub):
         self.repository = repository
+
+
+def test_only_one_instance_of_service_container_exists() -> None:
+    # arrange
+    sut: ServiceContainer = ServiceContainer.get_instance()
+
+    # act
+    instance2: ServiceContainer = ServiceContainer.get_instance()
+    instance3: ServiceContainer = ServiceContainer()
+
+    # assert
+    assert_that(instance2).is_equal_to(sut)
+    assert_that(instance3).is_equal_to(sut)
 
 def test_service_container_resolves_service_dependencies_automatically() -> None:
     # arrange
+    sut: ServiceContainer = ServiceContainer.get_instance()
 
     # act
-    desired_service: UseCaseStub = ServiceContainer.get(UseCaseStub)
+    desired_service: UseCaseStub = sut.get_service(UseCaseStub)
 
     # assert
     assert_that(desired_service).is_instance_of(UseCaseStub)
-    assert_that(desired_service.repository).is_instance_of(DummyRepository)
+    assert_that(desired_service.repository).is_instance_of(RepositoryStub)
+    assert_that(desired_service.repository.unit_of_work).is_instance_of(DummyUnitOfWork)
